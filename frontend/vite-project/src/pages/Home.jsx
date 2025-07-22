265656c38365a2eb6a6d1be8e48c40d0ec810681
harshilsonar <168526698+harshilsonar@users.noreply.github.com>
2025-07-22 19:18:41 +0530

    Add files via upload

---
+++ frontend/vite-project/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="welcome-container">
+  <h1>Welcome to Our Full Stack Project</h1>
+  <img src="https://media.giphy.com/media/3o7TKtnuHOHHUjR38Y/giphy.gif" alt="Welcome" />
+</div>
+
+  );
+}
+
+export default Home;
